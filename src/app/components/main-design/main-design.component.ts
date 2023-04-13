bc78b31fa98124db43a211068bf3ff6a58a58dda
Dana <dnabulsi5@gmail.com>
2023-04-13 22:47:35 +0300

    Create main-design.component.ts

---
+++ src/app/components/main-design/main-design.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-design',
+  templateUrl: './main-design.component.html',
+  styleUrls: ['./main-design.component.css']
+})
+export class MainDesignComponent {
+
+}
