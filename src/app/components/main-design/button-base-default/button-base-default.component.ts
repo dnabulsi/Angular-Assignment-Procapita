77a4fe61017bb857e7fba5bee854ec58fc1b57d9
Dana <dnabulsi5@gmail.com>
2023-04-13 22:44:23 +0300

    Create button-base-default.component.ts

---
+++ src/app/components/main-design/button-base-default/button-base-default.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-base-default',
+  templateUrl: './button-base-default.component.html',
+  styleUrls: ['./button-base-default.component.css']
+})
+export class ButtonBaseDefaultComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void { }
+}
