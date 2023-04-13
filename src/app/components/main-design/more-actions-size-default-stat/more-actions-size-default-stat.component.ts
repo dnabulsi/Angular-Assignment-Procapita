b41741372c15a7965bfce79237f212c1f7b29ed5
Dana <dnabulsi5@gmail.com>
2023-04-13 22:47:39 +0300

    Create more-actions-size-default-stat.component.ts

---
+++ src/app/components/main-design/more-actions-size-default-stat/more-actions-size-default-stat.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-more-actions-size-default-stat',
+  templateUrl: './more-actions-size-default-stat.component.html',
+  styleUrls: ['./more-actions-size-default-stat.component.css']
+})
+export class MoreActionsSizeDefaultStatComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void { }
+}
