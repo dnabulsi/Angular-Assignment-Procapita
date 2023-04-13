6385b5b7bbc1c427b38a3e9867f51d4b411f4606
Dana <dnabulsi5@gmail.com>
2023-04-13 22:46:21 +0300

    Create dropdown-size-default-state-default.component.ts

---
+++ src/app/components/main-design/dropdown-size-default-state-default/dropdown-size-default-state-default.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dropdown-size-default-state-default',
+  templateUrl: './dropdown-size-default-state-default.component.html',
+  styleUrls: ['./dropdown-size-default-state-default.component.css']
+})
+export class DropdownSizeDefaultStateDefaultComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
