79e73931a4b656ab5e7b412bec437da286552f7f
Dana <dnabulsi5@gmail.com>
2023-04-13 22:44:39 +0300

    Create divider-type-horizontal-spaced.component.ts

---
+++ src/app/components/main-design/divider-type-horizontal-spaced/divider-type-horizontal-spaced.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-divider-type-horizontal-spaced',
+  templateUrl: './divider-type-horizontal-spaced.component.html',
+  styleUrls: ['./divider-type-horizontal-spaced.component.css']
+})
+export class DividerTypeHorizontalSpacedComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
