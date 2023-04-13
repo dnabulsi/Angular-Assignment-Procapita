d20900bf8074dc353a9e6809c4fb02c8f3c1143f
Dana <dnabulsi5@gmail.com>
2023-04-13 22:47:02 +0300

    Create iconstyle-regular-padding-square.component.ts

---
+++ src/app/components/main-design/iconstyle-regular-padding-square/iconstyle-regular-padding-square.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-iconstyle-regular-padding-square',
+  templateUrl: './iconstyle-regular-padding-square.component.html',
+  styleUrls: ['./iconstyle-regular-padding-square.component.css']
+})
+export class IconstyleRegularPaddingSquareComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
