a64c0935ba3b4284c124e11eded821f3006e4546
Dana <dnabulsi5@gmail.com>
2023-04-13 22:46:58 +0300

    Create iconstyle-regular-padding-none.component.ts

---
+++ src/app/components/main-design/iconstyle-regular-padding-none/iconstyle-regular-padding-none.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-iconstyle-regular-padding-none',
+  templateUrl: './iconstyle-regular-padding-none.component.html',
+  styleUrls: ['./iconstyle-regular-padding-none.component.css']
+})
+export class IconstyleRegularPaddingNoneComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
