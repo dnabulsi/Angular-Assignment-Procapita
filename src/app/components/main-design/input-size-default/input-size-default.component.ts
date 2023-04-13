77a452f9582fa550884eaa7a4f256807a2fedcdf
Dana <dnabulsi5@gmail.com>
2023-04-13 22:47:25 +0300

    Create input-size-default.component.ts

---
+++ src/app/components/main-design/input-size-default/input-size-default.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-size-default',
+  templateUrl: './input-size-default.component.html',
+  styleUrls: ['./input-size-default.component.css']
+})
+export class InputSizeDefaultComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void { }
+}
