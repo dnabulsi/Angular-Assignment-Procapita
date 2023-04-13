45b5f371a437fc58191043e1a8ced5e865930088
Dana <dnabulsi5@gmail.com>
2023-04-13 22:47:30 +0300

    Create legal-entity-dropdown.component.ts

---
+++ src/app/components/main-design/legal-entity-dropdown/legal-entity-dropdown.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-legal-entity-dropdown',
+  templateUrl: './legal-entity-dropdown.component.html',
+  styleUrls: ['./legal-entity-dropdown.component.css']
+})
+export class LegalEntityDropdownComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
