bc90743d9d76d520c3dc0c9dcea534f5f0b14ec8
Dana <dnabulsi5@gmail.com>
2023-04-13 22:47:33 +0300

    Create main-design.component.spec.ts

---
+++ src/app/components/main-design/main-design.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MainDesignComponent } from './main-design.component';
+
+describe('MainDesignComponent', () => {
+  let component: MainDesignComponent;
+  let fixture: ComponentFixture<MainDesignComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MainDesignComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MainDesignComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
