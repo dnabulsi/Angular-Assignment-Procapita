d3bbd4444eb8cfbceec17dd75b00c996042271a7
Dana <dnabulsi5@gmail.com>
2023-04-13 22:44:34 +0300

    Create button-type-primary-size-default2.component.ts

---
+++ src/app/components/main-design/button-type-primary-size-default2/button-type-primary-size-default2.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-type-primary-size-default2',
+  templateUrl: './button-type-primary-size-default2.component.html',
+  styleUrls: ['./button-type-primary-size-default2.component.css']
+})
+export class ButtonTypePrimarySizeDefault2Component implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
