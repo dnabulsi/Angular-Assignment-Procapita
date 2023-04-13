dd7ed17e14c1d35eaf1336e6056ee0ec814d8cbe
Dana <dnabulsi5@gmail.com>
2023-04-13 22:44:16 +0300

    Create assessment-item-template-state.component.ts

---
+++ src/app/components/main-design/assessment-item-template-state/assessment-item-template-state.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assessment-item-template-state',
+  templateUrl: './assessment-item-template-state.component.html',
+  styleUrls: ['./assessment-item-template-state.component.css']
+})
+export class AssessmentItemTemplateStateComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void { }
+}
