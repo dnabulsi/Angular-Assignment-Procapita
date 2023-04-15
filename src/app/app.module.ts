54d21d2f4dbc2877f50a35ca59700a3c28a4d3f5
Dana <dnabulsi5@gmail.com>
2023-04-15 06:01:17 +0300

    Delete unnecessary component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { ButtonTypePrimarySizeDefaultComponent } from './components/button-type-
 import { ButtonTypePrimarySizeDefault2Component } from './components/button-type-primary-size-default2/button-type-primary-size-default2.component';
 import { DropdownSizeDefaultStateDefaultComponent } from './components/dropdown-size-default-state-default/dropdown-size-default-state-default.component';
 import { IconstyleRegularPaddingSquare2Component } from './components/iconstyle-regular-padding-square2/iconstyle-regular-padding-square2.component';
-import { IconstyleSolidPaddingSquareComponent } from './components/iconstyle-solid-padding-square/iconstyle-solid-padding-square.component';
 import { InputSizeDefaultComponent } from './components/input-size-default/input-size-default.component';
 import { LegalEntityDropdownComponent } from './components/legal-entity-dropdown/legal-entity-dropdown.component';
 import { MoreActionsSizeDefaultStatComponent } from './components/more-actions-size-default-stat/more-actions-size-default-stat.component';
@@ -29,7 +28,6 @@ import { IconstyleRegularPaddingSquareComponent } from './components/iconstyle-r
     ButtonTypePrimarySizeDefault2Component,
     DropdownSizeDefaultStateDefaultComponent,
     IconstyleRegularPaddingSquare2Component,
-    IconstyleSolidPaddingSquareComponent,
     InputSizeDefaultComponent,
     LegalEntityDropdownComponent,
     MoreActionsSizeDefaultStatComponent,
