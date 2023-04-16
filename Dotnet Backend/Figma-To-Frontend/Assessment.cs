375ff28198336a208cb674241754d8238e0383b0
Dana <dnabulsi5@gmail.com>
2023-04-16 07:46:58 +0300

    Added backend, restructured project

---
+++ Dotnet Backend/Figma-To-Frontend/Assessment.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Figma_To_Frontend
+{
+    public class Assessment
+    {
+        [Key]
+        public int Id { get; set; }
+        public bool Status { get; set; }
+        public string? Title {  get; set; }
+        public int Tests { get; set; }
+        public int Questions { get; set; }
+        public string? Country { get; set; }
+        public string? Industry { get; set; }
+        public string? JobFamily { get; set; }
+
+
+    }
+}
---
+++ Dotnet Backend/Figma-To-Frontend/Controllers/AssessmentsController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+using System.Diagnostics.Metrics;
+
+namespace Figma_To_Frontend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssessmentsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Assessment>>> GetAssessments()
+        {
+            return new List<Assessment>
+            {
+                new Assessment
+                {
+                    Id = 1,
+                    Status = true,
+                    Title = "HR Officer: Critical listening Skills",
+                    Tests = 3,
+                    Questions = 39,
+                    Country = "Kuwait",
+                    Industry = "Construction",
+                    JobFamily = "Blue collar",
+                },
+                new Assessment
+                {
+                    Id = 2,
+                    Status = false,
+                    Title = "HR Officer: Mediation Skills",
+                    Tests = 3,
+                    Questions = 24,
+                    Country = "Jordan",
+                    Industry = "Technology",
+                    JobFamily = "White collar",
+                },
+                new Assessment
+                {
+                    Id = 3,
+                    Status = true,
+                    Title = "HR Officer: Persuasion Skills",
+                    Tests = 4,
+                    Questions = 65,
+                    Country = "All Countries",
+                    Industry = "Management",
+                    JobFamily = "White collar",
+                },
+                new Assessment
+                {
+                    Id = 4,
+                    Status = false,
+                    Title = "HR Risk Assessment",
+                    Tests = 2,
+                    Questions = 18,
+                    Country = "Kuwait",
+                    Industry = "Technology",
+                    JobFamily = "White collar",
+                },
+                new Assessment
+                {
+                    Id = 5,
+                    Status = true,
+                    Title = "Hiring Manager: Communication Skills",
+                    Tests = 3,
+                    Questions = 48,
+                    Country = "Kuwait",
+                    Industry = "Technology",
+                    JobFamily = "White collar",
+                },
+            };
+        }
+    }
+
+}
